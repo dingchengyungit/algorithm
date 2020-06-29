9905c97a7a4652bae9fd83ac73fdd3315e1e699d
dingchengyun <dingchengyun@sdiread.com>
2020-06-29 21:23:58 +0800

    d

--- link/src/main/java/dcy/com/link/LinkTest.java
+++ link/src/main/java/dcy/com/link/LinkTest.java
@@ -43,9 +43,12 @@ public class LinkTest {
 //        printReverseNode(n1);
 
         // 4
-        Node x = merge2SortedNode(n11, n21);
-        System.out.println(x);
+//        Node x = merge2SortedNode(n11, n21);
+//        System.out.println(x);
 
+        // 5
+        Node rk = removeReverseKNode(n11,2);
+        System.out.println(rk);
     }
 
     /**
@@ -196,6 +199,54 @@ public class LinkTest {
         }
         if (n2 != null) {
             cur.next = n2;
+        }
+
+        return head;
+    }
+
+    /**
+     * 删除倒数第K个节点
+     * @param head
+     * @param k
+     * @return
+     */
+    public static Node removeReverseKNode(Node head, int k) {
+        if (head == null || k < 0) {
+            return null;
+        }
+
+        // 获取链表长度
+        int nodeLen = 0;
+        Node lenNode = head;
+        while (lenNode != null) {
+            nodeLen++;
+            lenNode = lenNode.next;
+        }
+
+        // 倒数第k个的index
+        int kIndex = nodeLen - k;
+        if (kIndex < 0) {
+            return null;
+        }
+
+        if (kIndex == 0) {
+            head = head.next;
+            return head;
+        }
+
+        // 找到第k个节点，把k的前一个节点的next指向k节点的next
+        int i = 1;
+        Node pre = head;
+        Node cur = head.next;
+
+        while (cur != null) {
+            if (i == kIndex) {
+                pre.next = cur.next;
+                break;
+            }
+            pre = pre.next;
+            cur = cur.next;
+            i++;
         }
 
         return head;
